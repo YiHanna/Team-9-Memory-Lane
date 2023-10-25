a4bb307def757eae9f2640acee17f3f6e1c4dc04
Hanna <yihanl@andrew.cmu.edu>
2023-10-25 16:15:27 -0400

    Created App

---
+++ Memory Lane/Memory Lane/Memory_LaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_LaneApp.swift
+//  Memory Lane
+//
+//  Created by Hanna Luo on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Memory_LaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
