33716f51509362e8f139e4ef7fad21924942f955
Sunny Liang <sunnyliang6@gmail.com>
2023-12-10 05:07:18 -0500

    enhance UI of add post view

--- MemoryLane/MemoryLane/Views/AddPostView.swift
+++ MemoryLane/MemoryLane/Views/AddPostView.swift
@@ -11,30 +11,40 @@ import Firebase
 import FirebaseFirestore
 
 struct AddPostView: View {
-    var showPrompt : Bool
-    @State var showImagePicker: Bool = false
-    @State var image: UIImage? = nil
-  
-    @State var description: String = ""
-    @State var selectedDate = Date()
-    
-    @ObservedObject var viewModel = LocationViewModel()
-    
-    @State private var isPostCreated = false
-    @State private var isPopoverPresented = false
+  var showPrompt : Bool
+  @State var showImagePicker: Bool = false
+  @State var image: UIImage? = nil
 
-    @Environment(\.defaultMinListRowHeight) var minRowHeight
-    
-    var displayImage: Image? {
-      if let picture = image {
-        return Image(uiImage: picture)
-      } else {
-        return nil
-      }
+  @State var description: String = ""
+  @State var selectedDate = Date()
+  
+  @ObservedObject var viewModel = LocationViewModel()
+  
+  @State private var isPostCreated = false
+  @State private var isPopoverPresented = false
+
+  @Environment(\.defaultMinListRowHeight) var minRowHeight
+  @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+  
+  var displayImage: Image? {
+    if let picture = image {
+      return Image(uiImage: picture)
+    } else {
+      return nil
     }
+  }
+  
+  var btnBack: some View {
+    Button(action: {
+      presentationMode.wrappedValue.dismiss()
+    }) {
+      Image(systemName: "chevron.backward")
+        .foregroundColor(Color.brown)
+    }
+  }
   
   var body: some View {
-    NavigationView{
+    NavigationView {
       ScrollView {
         ZStack {
           VStack{
@@ -44,28 +54,53 @@ struct AddPostView: View {
               }
             }
             displayImage?.resizable().scaledToFit().padding()
-            Button(action: {
-              self.showImagePicker = true
-            }) {
-              Text("Add Picture")
-            }.padding()
+            
+            HStack {
+              Button(action: {
+                self.showImagePicker = true
+              }) {
+                Text("Add Picture")
+                  .padding()
+                  .background(Color.taupe)
+                  .foregroundColor(.white)
+                  .cornerRadius(10)
+              }
+              .padding()
+              
+              Spacer()
+            }
+            
             VStack {
-              Text("Description (max: 250 characters):")
+              Text("Description (up to 250 characters)")
                 .fontWeight(.bold)
                 .padding(.trailing)
+                .frame(maxWidth: .infinity, alignment: .leading)
               TextField("Add a caption", text: $description)
+                .foregroundColor(Color.taupe)
                 .padding(.trailing)
+                .frame(maxWidth: .infinity, alignment: .leading)
             }.padding()
+            
             VStack {
-              Text("Date:")
+              Text("Date")
                 .fontWeight(.bold)
                 .padding(.trailing)
-              DatePicker("Add a month and year", selection: $selectedDate, in: ...Date(), displayedComponents: .date)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+              DatePicker("Select an approximate date", selection: $selectedDate, in: ...Date(), displayedComponents: .date)
+                .foregroundColor(.taupe)
                 .datePickerStyle(DefaultDatePickerStyle())
+                .frame(maxWidth: .infinity, alignment: .leading)
             }.padding()
+            
             VStack {
+              Text("Location")
+                .fontWeight(.bold)
+                .padding([.leading, .trailing])
+                .frame(maxWidth: .infinity, alignment: .leading)
+              
               TextField("Search for a location", text: $viewModel.searchQuery)
-                .padding()
+                .padding([.leading, .trailing])
                 .textFieldStyle(RoundedBorderTextFieldStyle())
               if viewModel.showLocationResults {
                 List(viewModel.searchResults, id: \.title) { result in
@@ -85,57 +120,61 @@ struct AddPostView: View {
           .fullScreenCover(isPresented: $showImagePicker) {
             PhotoCaptureView(showImagePicker: self.$showImagePicker, image: self.$image)
           }
-          .navigationBarTitle("New Memory")
-          .navigationBarItems(trailing:
-                                Button(action: {
-            createPost()
-            isPostCreated = true
-          }) {
-            Text("Post")
-          }
-          )
           .background(
             NavigationLink("", destination: HomeView(dbDocuments: dbDocuments), isActive: $isPostCreated)
           )
-        }.navigationBarBackButtonHidden(false)
+        }
       }
     }
+    .navigationBarTitle("New Memory", displayMode: .inline)
+    .navigationBarBackButtonHidden(true)
+    .navigationBarItems(
+      leading: btnBack,
+      trailing:
+        Button(action: {
+            createPost()
+            isPostCreated = true
+        }) {
+          Text("Post")
+            .foregroundColor(Color.brown)
+        }
+    )
   }
         
     
-    private func createPost(){
-        var data : [String: Any] = [
-            "date": Timestamp(date: selectedDate),
-            "description": description,
-            "num_likes": 0,
-        ]
-         
-        viewModel.getGeoPoint(){ geoPoint in
-            data["location"] = geoPoint
-            
-            if let img = image {
-                dbDocuments.uploadImage(img) { (url) in
-                    if let imageURL = url {
-                        data["photo"] = imageURL.absoluteString
-                        print("Image URL: \(imageURL.absoluteString)")
-                    } else {
-                        print("Failed to upload the image.")
-                    }
+  private func createPost(){
+      var data : [String: Any] = [
+          "date": Timestamp(date: selectedDate),
+          "description": description,
+          "num_likes": 0,
+      ]
+       
+      viewModel.getGeoPoint(){ geoPoint in
+          data["location"] = geoPoint
+          
+          if let img = image {
+              dbDocuments.uploadImage(img) { (url) in
+                  if let imageURL = url {
+                      data["photo"] = imageURL.absoluteString
+                      print("Image URL: \(imageURL.absoluteString)")
+                  } else {
+                      print("Failed to upload the image.")
+                  }
 //                        print("data dict before creating post:")
 //                        print(data)
-                    
-                    dbDocuments.createPost(data: data)
-                }
-            }else{
-                print("creating post without picture")
-                dbDocuments.createPost(data: data)
-            }
-        }
-    }
+                  
+                  dbDocuments.createPost(data: data)
+              }
+          }else{
+              print("creating post without picture")
+              dbDocuments.createPost(data: data)
+          }
+      }
+  }
 }
 
-struct AddPostView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddPostView(showPrompt: false)
-    }
-}
+//struct AddPostView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AddPostView(showPrompt: false)
+//    }
+//}
