661f82240f0d407587641db257033c82d5d44e8b
Sunny Liang <sunnyliang6@gmail.com>
2023-12-10 22:59:46 -0500

    fix comment UI

--- MemoryLane/MemoryLane/Views/CommentRowView.swift
+++ MemoryLane/MemoryLane/Views/CommentRowView.swift
@@ -7,20 +7,35 @@
 
 import Foundation
 import SwiftUI
+import FirebaseFirestoreSwift
 
 struct CommentRowView: View {
   var comment : Comment
   @State var username = ""
   
   var body: some View {
-    HStack{
+    VStack(alignment: .leading) {
       Text(username)
-      Text(":")
-      Text(comment.text).foregroundColor(Color.black)
-    }.onAppear{getUsername()}
+        .font(.system(size: 15))
+      Text("\(formattedTime(comment: comment))")
+        .font(.system(size: 15))
+        .foregroundColor(.taupe)
+      Text(comment.text)
+        .font(.system(size: 15))
+    }
+    .onAppear{getUsername()}
+    .padding()
+    .frame(width: 360, alignment: .leading)
   }
   
   private func getUsername() {
       username = dbDocuments.getUserName(user_id: comment.user_id) ?? "username not found"
+  }
+  
+  func formattedTime(comment: Comment) -> String {
+      let date = comment.time.dateValue()
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "MMM d, yyyy hh:mm a"
+      return dateFormatter.string(from: date)
   }
 }
